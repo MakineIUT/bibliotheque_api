1d4fcda38f98d74af57d2b69f000e0829a549d64
MakineIUT <115160757+MakineIUT@users.noreply.github.com>
2025-12-10 09:03:16 +0100

    Add files via upload

---
+++ demo/src/test/java/Makine/IUT/demo/BibliothequeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Makine.IUT.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothequeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
